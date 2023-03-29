50dce61678e538bfd9dcac2af9e65f32ec93b5d6
xinyaowang <791157327nn@gmail.com>
2023-03-29 21:26:43 +0900

    car_ops result

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+import os
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = "static/user_photo"
 
 
 @app.route('/')
@@ -12,9 +15,30 @@ def index():  # put application's code here
 def photo():
     return render_template('photos.html')
 
+
 @app.route('/help')
 def help():
     return render_template('help.html')
+
+
+@app.route('/upload', methods=["POST", "GET"])
+def upload():
+    if request.method == "POST":
+        f = request.files['file']
+        if f:
+
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+            return render_template("car_ops.html")
+        else:
+
+            return render_template("photos.html")
+
+
+@app.route('/op_upload', methods=["POST"])
+def op_upload():
+    op_list = request.form.getlist('car_op')
+    print(op_list)
+    return render_template("result.html")
 
 
 if __name__ == '__main__':
