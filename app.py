2ece1a572eb328504902a7c332cb87f008f0c19e
xinyaowang <791157327nn@gmail.com>
2023-03-29 17:38:05 +0900

    help and photos

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
     return render_template('index.html')
+
+
+@app.route('/photos')
+def photo():
+    return render_template('photos.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
 
 
 if __name__ == '__main__':
